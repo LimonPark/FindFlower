1ec350f39df318bc29b7b820b2be72331f258246
nikita <kafes-nik@mail.ru>
2023-10-17 21:53:11 +0300

    Adding imagePicker

--- FindFlower/ViewController.swift
+++ FindFlower/ViewController.swift
@@ -7,13 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    let imagePicker = UIImagePickerController()
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = .camera
     }
 
-
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let userPickedImage = info[UIImagePickerController.InfoKey.editedImage]
+        
+        imageView.image = userPickedImage as? UIImage
+        
+        imagePicker.dismiss(animated: true)
+        
+    }
+    
+    @IBAction func cameraPressed(_ sender: UIBarButtonItem) {
+    }
+    
 }
 
